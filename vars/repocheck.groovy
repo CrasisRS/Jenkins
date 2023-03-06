40e3710a1ffa19031ee0bf65cea487e29e0c8633
Rudolf Soller <rudolf.soller11@web.de>
2023-03-06 11:33:29 +0100

    fix indentation

--- vars/repocheck.groovy
+++ vars/repocheck.groovy
@@ -5,19 +5,20 @@ def timeoutSeconds = 60
 def intervalSeconds = 5
 def startTime = System.currentTimeMillis()
 def call(){
-while ((System.currentTimeMillis() - startTime) / 1000 < timeoutSeconds) {
-    try {
-        def connection = new URL(gitHubUrl).openConnection() as HttpURLConnection
-        connection.setRequestMethod("HEAD")
-        if ( connection.responseCode == HttpURLConnection.HTTP_OK) {
-            println "Connection successful!"
-            return true
-        }
-    } catch (Exception e) {
+    while ((System.currentTimeMillis() - startTime) / 1000 < timeoutSeconds) {
+        try {
+            def connection = new URL(gitHubUrl).openConnection() as HttpURLConnection
+            connection.setRequestMethod("HEAD")
+            if ( connection.responseCode == HttpURLConnection.HTTP_OK) {
+                println "Connection successful!"
+                return true
+            }
+        } catch (Exception e) {
         //Ignore
-    }
-    println "Connection unsuccesful. REtrying in ${intervalSeconds} seconds"
-    Threat.sleep(intervalSeconds * 1000)
-} println "Connection unsuccessful after ${timeoutSeconds} seconds."
-return false
+        }
+        println "Connection unsuccesful. REtrying in ${intervalSeconds} seconds"
+        Threat.sleep(intervalSeconds * 1000)
+    } 
+    println "Connection unsuccessful after ${timeoutSeconds} seconds."
+    return false
 }
