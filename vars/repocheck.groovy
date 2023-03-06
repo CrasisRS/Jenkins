b6e74f4c337575e2564c005f912d9e3fb2a39104
Rudolf Soller <rudolf.soller11@web.de>
2023-03-06 11:41:26 +0100

    try to fix property error

--- vars/repocheck.groovy
+++ vars/repocheck.groovy
@@ -1,10 +1,13 @@
 import java.net.URL
 import java.net.HttpURLConnection 
+
+def call(){
+    
 def gitHubUrl = "https://github.com/CrasisRS/for_testing_only.git"
 def timeoutSeconds = 60
 def intervalSeconds = 5
-startTime = System.currentTimeMillis()
-def call(){
+def startTime = System.currentTimeMillis()
+
     while ((System.currentTimeMillis() - startTime) / 1000 < timeoutSeconds) {
         try {
             def connection = new URL(gitHubUrl).openConnection() as HttpURLConnection
